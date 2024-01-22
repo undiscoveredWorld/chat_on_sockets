1933af09ecd72cad75978a61aa6fc4927a1dd202
undiscoveredWorld <newspaw1111@mail.ru>
2024-01-22 13:16:04 +0000

    Refactoring

--- server.py
+++ server.py
@@ -13,12 +13,13 @@ List of functions:
     event_loop
 
 List of constants:
+    HOST
     PORT
-    COUNT_OF_CLIENTS
 
 List of variables:
     users
     message_history
+    count_of_clients
     server_socket
 """
 
@@ -30,15 +31,16 @@ from typing import Callable
 
 HOST: str = ''
 PORT: int = 9090
-COUNT_OF_CLIENTS: int = 4
-SERVER_SOCKET: socket = socket()
 
-_names_list: list[str] = ["John", "Jill", "Smith", "Bella"]
+_names: list[str] = ["John", "Jill", "Smith", "Bella"]
 users: list["User"] = []
-message_history: list["Message"] = []
+sent_messages: list["Message"] = []
+count_of_clients: int = len(_names)
 
-connection_selector = DefaultSelector()
-get_message_selector = DefaultSelector()
+server_socket: socket = socket()
+
+_connection_selector = DefaultSelector()
+_get_message_selector = DefaultSelector()
 
 
 @dataclass
@@ -56,15 +58,6 @@ class User:
 
     client_socket: socket
     name: str
-
-    def __init__(self, client_socket: socket):
-        """
-        Set all variables to arguments.
-
-        :param client_socket: client socket of user
-        """
-        self.client_socket = client_socket
-        self.name = _generate_name()
 
 
 @dataclass
@@ -95,18 +88,12 @@ class _Handler:
     args: list
 
 
-def check_free_positions() -> bool:
-    """
-    Return true, if there are free positions, otherwise return false.
-
-    :return: true, if there are free positions, otherwise return false
-    """
-    return len(_names_list) != 0
-
-
 def _accept_connection():
     """
-    Accept connection from client, send message history to it.
+    Procedure to accept connection.
+
+    Procedure accepts connection, creates and connects user, send user some messages
+    and then creates handler for reading messages
 
     Side effects:
         new user will append in users
@@ -118,97 +105,62 @@ def _accept_connection():
     """
     while True:
         yield
-
         client_socket: socket
-        client_socket, _ = SERVER_SOCKET.accept()
+        client_socket, _ = server_socket.accept()
 
+        # Handle no free positions case
         if not check_free_positions():
-            send_message(client_socket, "Server is full. You will disconnect\n")
+            send_message(
+                client_socket,
+                "Server is full. You will disconnect\n"
+            )
             logging.info("Got new connection, but server is full")
             client_socket.close()
             continue
 
-        new_user = User(client_socket)
+        new_user = _create_user(client_socket)
         users.append(new_user)
-        logging.info("Accepted new connection. Places left: %d", len(_names_list))
-        logging.info("%s:%d connected", *client_socket.getpeername())
 
         send_message(client_socket, f"Your name: {new_user.name}\n")
         send_message_history(client_socket)
 
+        # Subscribe on read socket event
         get_message = _get_message(client_socket)
         handler = _Handler(next, [get_message])
-        get_message_selector.register(client_socket, EVENT_READ, data=handler)
+        _get_message_selector.register(client_socket, EVENT_READ, data=handler)
+
+        logging.info(
+            "Accepted new connection. Places left: %d",
+            len(_names)
+        )
+        logging.info("%s:%d connected", *client_socket.getpeername())
 
 
-def _generate_name() -> str:
+def check_free_positions() -> bool:
     """
-    Generate a random name.
+    Return true, if there are free positions, otherwise return false.
 
-    :return: generated name
+    :return: true, if there are free positions, otherwise return false
     """
-    return _names_list.pop()
+    return len(_names) != 0
 
 
-def get_user(client_socket: socket) -> User:
+def _create_user(client_socket: socket) -> User:
     """
-    Get user from socket.
+    Create user from client_socket.
 
-    User is searched in users
+    Side effects:
+        Pop name from _names
 
-    :param client_socket: socket object for search
-    :raises: ValueError if user doesn't exist
-    :return: searched user or None
+    :param client_socket:
+    :return:
     """
-    for user in users:
-        if user.client_socket == client_socket:
-            return user
-    else:
-        raise ValueError("No user found")
-
-
-def del_user(user: User) -> None:
-    """
-    Delete user.
-
-    Deletes user from users, returns name in _names_list, and closed client
-    socket
-    """
-    users.remove(user)
-    _names_list.insert(0, user.name)
-    user.client_socket.close()
-
-
-def _get_message(client_socket: socket):
-    while True:
-        yield
-        message_str = client_socket.recv(4096)
-
-        if not message_str:
-            get_message_selector.unregister(client_socket)
-
-            user = get_user(client_socket)
-            if user:
-                del_user(user)
-
-            client_socket.close()
-
-            logging.info("Client disconnected. Places left: %d", len(_names_list))
-        else:
-            user = get_user(client_socket)
-            if not user:
-                return
-
-        message = Message(message_str.decode(), user)
-        message_history.append(message)
-
-        for user_in_list in users:
-            send_message(
-                user_in_list.client_socket,
-                f"{user.name}:{message_str.decode()}"
-            )
-
-        logging.info("Message got")
+    name = _names.pop()
+    user = User(
+        client_socket=client_socket,
+        name=name
+    )
+    return user
 
 
 def send_message(client_socket: socket, message: str) -> None:
@@ -229,8 +181,100 @@ def send_message_history(client_socket: socket) -> None:
     :param client_socket: client for getting message history
     :return: None
     """
-    for message in message_history:
+    for message in sent_messages:
         send_message(client_socket, f"{message.sender.name}:{message.body}")
+
+
+def _get_message(client_socket: socket):
+    """
+    Procedure to receive messages from client socket.
+
+    if message isn't sent, user disconnect
+    This procedure receives message from client socket
+
+    :param client_socket:
+    :return:
+    """
+    while True:
+        yield
+        message_str = client_socket.recv(4096)
+
+        if not message_str:
+            _disconnect_socket(client_socket)
+            return
+
+        try:
+            user = get_user(client_socket)
+        except ValueError:
+            _disconnect_socket(client_socket)
+            return
+
+        message = Message(message_str.decode(), user)
+        sent_messages.append(message)
+
+        for user_in_list in users:
+            send_message(
+                user_in_list.client_socket,
+                f"{user.name}:{message_str.decode()}"
+            )
+
+        logging.info("Message got")
+
+
+def _unsubscribe_socket(client_socket: socket):
+    _get_message_selector.unregister(client_socket)
+    client_socket.close()
+
+
+def _disconnect_socket(client_socket: socket) -> None:
+    try:
+        user = get_user(client_socket)
+        return_name(user.name)
+        del_user(user)
+    except ValueError:
+        pass
+
+    _unsubscribe_socket(client_socket)
+
+    logging.info(
+        "Client disconnected. Places left: %d",
+        len(_names)
+    )
+
+
+def get_user(client_socket: socket) -> User:
+    """
+    Get user from socket.
+
+    User is searched in users
+
+    :param client_socket: socket object for search
+    :raises: ValueError if user doesn't exist
+    :return: searched user or None
+    """
+    for user in users:
+        if user.client_socket == client_socket:
+            return user
+
+    raise ValueError("No user found")
+
+
+def del_user(user: User) -> None:
+    """
+    Delete user.
+
+    Deletes user from users, returns name in _names_list, and closed client
+    socket
+    """
+    try:
+        users.remove(user)
+    except ValueError:
+        pass
+
+
+def return_name(name: str):
+    if name not in _names:
+        _names.insert(0, name)
 
 
 def event_loop() -> None:
@@ -240,30 +284,30 @@ def event_loop() -> None:
     :return: None
     """
     while True:
-        connection_keys = connection_selector.select(0.5)
+        connection_keys = _connection_selector.select(0.5)
         for key, _ in connection_keys:
             func = key.data.function
             func(*key.data.args)
 
-        get_message_keys = get_message_selector.select(1)
+        get_message_keys = _get_message_selector.select(1)
         for key, _ in get_message_keys:
             func = key.data.function
             func(*key.data.args)
 
 
 if __name__ == "__main__":
-    SERVER_SOCKET.bind(("0.0.0.0", PORT))
-    SERVER_SOCKET.listen(COUNT_OF_CLIENTS)
+    server_socket.bind(("0.0.0.0", PORT))
+    server_socket.listen(count_of_clients)
 
     logging.basicConfig(level=logging.INFO)
 
     logging.info("Server is listening on port %d", PORT)
-    logging.info("Server can accept %s connections", COUNT_OF_CLIENTS)
+    logging.info("Server can accept %s connections", count_of_clients)
 
     accept = _accept_connection()
 
-    connection_selector.register(
-        SERVER_SOCKET,
+    _connection_selector.register(
+        server_socket,
         EVENT_READ,
         data=_Handler(next, [accept])
     )
